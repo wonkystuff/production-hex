1fea7e68119d9acc43953bf571ab1f40b7558636
namke <john@namke.com>
2024-03-24 19:29:17 +0000

    Just use avrdude

--- prog.py
+++ prog.py
@@ -10,13 +10,11 @@ parts = hex_file.split(".")[0].split("_")
 print(parts)
 
 device = parts[1].lower()
-home_dir = os.path.expanduser("~")
 
-progpy = glob.glob(
-    os.path.join(home_dir, "Library", "Arduino15", "**/prog.py"), recursive=True
-)[0]
+# avrdude must be v7.0 or more to support serialupdi
+# if it's not already on the path, then add it like this when invoking this script:
+# PATH=$PATH:<path to avrdude> python3 prog.py <hex file> <serial port>
 
-# Use avrdude if it's installed (v7.0 or more supports serialupdi):
 subprocess.call(
     [
         "avrdude",
@@ -33,25 +31,3 @@ subprocess.call(
         "flash:w:"+hex_file+":i",
     ]
 )
-
-# or use prog.py from the 'megaTinyCore' package
-# subprocess.call(
-#     [
-#         "python3",
-#         "-u",
-#         progpy,
-#         "-t",
-#         "uart",
-#         "-u",
-#         serialPort,
-#         "-b",
-#         "57600",
-#         "-d",
-#         DEVICE,
-#         "-f",
-#         hex_file,
-#         "-a",
-#         "write",
-#         "-v",
-#     ]
-# )
